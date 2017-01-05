999c3d2802c0518fa8ffeb437154b894b26b5d71
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-05 13:01:04 -0800

    added error message for missing file

--- calculator_file.py
+++ calculator_file.py
@@ -6,36 +6,45 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
-file_name = "numbers.txt"
-with open(file_name) as f:
-    tokens = f.readlines()
-    print(tokens)
+def calculator():
+    file_name = "numbers.txt"
+    try:
+        with open(file_name) as f:
+            tokens = f.readlines()
+        f.close()
+    except (OSError, IOError) as e:
+        print("The file numbers.txt doesn\'t exist!")
+        return
 
+    tokens[0] = tokens[0].rstrip()
 
-arg3 = ["+", "-", "*", "/", "pow", "mod"]
-arg2 = ["square", "cube"]
-if ((tokens[0] in arg3 and len(tokens) < 3) or
-    (tokens[0] in arg2 and len(tokens) != 2)):
-    print("Wrong number of arguments. Check your file")
-else:
+    arg3 = ["+", "-", "*", "/", "pow", "mod"]
+    arg2 = ["square", "cube"]
 
-    if tokens[0] == "+":
-        print('{0:.2f}'.format(reduce((lambda x,y: add(int(x), int(y))), tokens[1:])))
-    elif tokens[0] == "-":
-        print('{0:.2f}'.format(reduce((lambda x,y: subtract(int(x), int(y))), tokens[1:])))
-    elif tokens[0] == "*":
-        print('{0:.2f}'.format(reduce((lambda x,y: multiply(int(x), int(y))), tokens[1:])))
-    elif tokens[0] == "/":
-        print('{0:.2f}'.format(reduce((lambda x,y: divide(int(x), int(y))), tokens[1:])))
-    elif tokens[0] == "square":
-        print('{0:.2f}'.format(square(int(tokens[1]))))
-    elif tokens[0] == "cube":
-        print('{0:.2f}'.format(cube(int(tokens[1]))))
-    elif tokens[0] == "pow":
-        print('{0:.2f}'.format(reduce((lambda x,y: power(int(x), int(y))), tokens[1:])))
-    elif tokens[0] == "mod":
-        print('{0:.2f}'.format(reduce((lambda x,y: mod(int(x), int(y))), tokens[1:])))
+    if ((tokens[0] in arg3 and len(tokens) < 3) or
+        (tokens[0] in arg2 and len(tokens) != 2)):
+        print("Wrong number of arguments. Check your file")
     else:
-        print("I don\'t understand. Check your file")
+        result = open("result.txt","w")
+        if tokens[0] == "+":
+            result.write('{0:.2f}'.format(reduce((lambda x,y: add(int(x), int(y))), tokens[1:])))
+        elif tokens[0] == "-":
+            result.write('{0:.2f}'.format(reduce((lambda x,y: subtract(int(x), int(y))), tokens[1:])))
+        elif tokens[0] == "*":
+            result.write('{0:.2f}'.format(reduce((lambda x,y: multiply(int(x), int(y))), tokens[1:])))
+        elif tokens[0] == "/":
+            result.write('{0:.2f}'.format(reduce((lambda x,y: divide(float(x), float(y))), tokens[1:])))
+        elif tokens[0] == "square":
+            result.write('{0:.2f}'.format(square(int(tokens[1]))))
+        elif tokens[0] == "cube":
+            result.write('{0:.2f}'.format(cube(int(tokens[1]))))
+        elif tokens[0] == "pow":
+            result.write('{0:.2f}'.format(reduce((lambda x,y: power(int(x), int(y))), tokens[1:])))
+        elif tokens[0] == "mod":
+            result.write('{0:.2f}'.format(reduce((lambda x,y: mod(int(x), int(y))), tokens[1:])))
+        else:
+            print("I don\'t understand. Check your file")
 
-f.close()
+    result.close()
+
+calculator()
